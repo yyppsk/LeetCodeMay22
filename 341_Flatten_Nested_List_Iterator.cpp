86027a79535d213f82bb1c04518aa110d63c84b2
Pranjal Pratap Singh <pranjalpratapsingh1@gmail.com>
2022-05-08 23:03:01 +0530

    CodeUpdate

--- 341_Flatten_Nested_List_Iterator.cpp
+++ 341_Flatten_Nested_List_Iterator.cpp
@@ -1,11 +1,32 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-using namespace std;
-void solve(){
-
-}
-int main()
-{
-    return 0;
-}
+class NestedIterator {
+    vector<int> flat;
+    int i = 0;
+public:
+    void helper(vector<NestedInteger> &nestedList){
+        for(int i = 0; i < nestedList.size(); i++){
+            if(nestedList[i].isInteger()){
+                flat.push_back(nestedList[i].getInteger());
+            }else{
+                helper(nestedList[i].getList());
+            }
+        }
+    }
+    NestedIterator(vector<NestedInteger> &nestedList) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        helper(nestedList);
+    }
+    
+    int next() {
+        int value = flat.at(i);
+        i++;
+        return value;
+    }
+    
+    bool hasNext() {
+        if(i<flat.size())
+            return true;
+        else
+            return false;
+    }
+};
