334ce7d1438ab7b9ee6c7e25f28b23f78d12595c
Pranjal Pratap Singh <pranjalpratapsingh1@gmail.com>
2022-05-11 22:40:39 +0530

    Changes

--- 1641_Count_Sorted_Vowel_Strings.cpp
+++ 1641_Count_Sorted_Vowel_Strings.cpp
@@ -2,11 +2,14 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+void generate(vector<int> sets, )
 int countVowelStrings(int n)
-{
-    
+{   vector<char> vowels = {'a','e','i','o','u'};
+    vector<int> sets;
+    generate(sets,n,vowels);
 }
 int main()
-{
+{   
+    countVowelStrings(2);
     return 0;
 }
--- 216_Combination_Sum_III.cpp
+++ 216_Combination_Sum_III.cpp
@@ -2,93 +2,75 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-vector<vector<int>> result;
-vector<vector<int>> ansArr;
-    int sum(vector<int>& combination){
-        int ans = 0;
-        for(auto x: combination){
-            ans += x;
-        }
-        return ans;
+vector<vector<int>> subsetss;
+void generate(vector<int> &subset, int i, int k, int n)
+{
+    if (k == subset.size() && n == 0)
+    {
+        subsetss.push_back(subset);
+        return;
     }
-    
-    void combinationSumHelper(int k, int n, vector<int>& combination, int num){
-        
-        if(k == 0){
-            if(sum(combination) == n ){
-                ansArr.push_back(combination);
-            }
-            return;
-        }
-        
-        if(num > 9) return;
-        
-        combination.push_back(num);  // Including num in combination
-        combinationSumHelper(k - 1, n, combination, num + 1);
-        combination.pop_back(); // Not including num in combination
-        combinationSumHelper(k, n, combination, num + 1);    
-        
-    }
+    if (i >= 10 || (n != 0 && i > n))
+        return;
+    generate(subset, i + 1, k, n);
+    subset.push_back(i);
+    generate(subset, i + 1, k, n - i);
+    subset.pop_back();
+}
 vector<vector<int>> combinationSum3(int k, int n)
 {
     if (n > 45)
     {
-        result.push_back({});
-        return result;
+        return subsetss;
     }
-    if (k == 1 && n > 9 || n < 1)
+    if (k == 1 && n > 9 && n < 1)
     {
-        result.push_back({});
-        return result;
+        subsetss.push_back({});
+        return subsetss;
     }
-    if (k == 2 && n > 17 || n < 3)
+    if (k == 2 && n > 17 && n < 3)
     {
-        result.push_back({});
-        return result;
+        subsetss.push_back({});
+        return subsetss;
     }
-    if (k == 3 && n > 24 || n < 6)
+    if (k == 3 && n > 24 && n < 6)
     {
-        result.push_back({});
-        return result;
+        subsetss.push_back({});
+        return subsetss;
     }
-    if (k == 4 && n > 30 || n < 10)
+    if (k == 4 && n > 30 && n < 10)
     {
-        result.push_back({});
-        return result;
+        subsetss.push_back({});
+        return subsetss;
     }
-    if (k == 5 && n > 35 || n < 15)
+    if (k == 5 && n > 35 && n < 15)
     {
-        result.push_back({});
-        return result;
+        subsetss.push_back({});
+        return subsetss;
     }
-    if (k == 6 && n > 39 || n < 21)
+    if (k == 6 && n > 39 && n < 21)
     {
-        result.push_back({});
-        return result;
+        subsetss.push_back({});
+        return subsetss;
     }
-    if (k == 7 && n > 42 || n < 28)
+    if (k == 7 && n > 42 && n < 28)
     {
-        result.push_back({});
-        return result;
+        subsetss.push_back({});
+        return subsetss;
     }
-    if (k == 8 && n > 44 || n < 36)
+    if (k == 8 && n > 44 && n < 36)
     {
-        result.push_back({});
-        return result;
+        subsetss.push_back({});
+        return subsetss;
     }
-    if (k == 9 && n > 45 || n < 45)
+    if (k == 9 && n > 45 && n < 45)
     {
-        result.push_back({});
-        return result;
+        subsetss.push_back({});
+        return subsetss;
     }
-    if (k == 1 && n <= 9)
-    {
-        result.push_back({k});
-        return result;
-    }
-    vector<int> combination;
-        combinationSumHelper(k, n, combination, 1);
-        return ansArr;
+    vector<int> subset;
+    generate(subset, 1, k, n);
+    return subsetss;
 }
 int main()
 {
