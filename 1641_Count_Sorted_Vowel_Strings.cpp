89c5e8543af4068679bd6ba49b28990247712539
Pranjal Pratap Singh <pranjalpratapsingh1@gmail.com>
2022-05-12 00:01:25 +0530

    1641. Count Sorted Vowel Strings

--- 1641_Count_Sorted_Vowel_Strings.cpp
+++ 1641_Count_Sorted_Vowel_Strings.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
 #include <vector>
+#include <numeric>
 #include <algorithm>
 using namespace std;
-void generate(vector<int> sets, )
 int countVowelStrings(int n)
-{   vector<char> vowels = {'a','e','i','o','u'};
-    vector<int> sets;
-    generate(sets,n,vowels);
+{
+    vector<string> vowels = {"a", "e", "i", "o", "u"};
+    vector<vector<int>> generator(n + 1, vector<int>(5, 0));
+    if (n == 1)
+        return 5;
+    for (int j = 0; j < 5; j++)
+    {
+        generator[0][j] = 1;
+    }
+    for (int i = 1; i <= n; i++)
+        for (int j = 0; j < 5; j++)
+            generator[i][j] = accumulate(generator[i - 1].begin(), generator[i - 1].begin() + j + 1, 0);
+    return generator[n][4];
 }
 int main()
-{   
-    countVowelStrings(2);
+{
+    cout << countVowelStrings(2);
     return 0;
 }
