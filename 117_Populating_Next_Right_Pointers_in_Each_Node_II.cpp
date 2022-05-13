670219862a2d170db507799d14ccea98c5f8ebd7
Pranjal Pratap Singh <pranjalpratapsingh1@gmail.com>
2022-05-13 23:49:04 +0530

    117. Populating Next Right Pointers in Each Node II

--- 117_Populating_Next_Right_Pointers_in_Each_Node_II.cpp
+++ 117_Populating_Next_Right_Pointers_in_Each_Node_II.cpp
@@ -1,10 +1,37 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-void solve(){
-
-}
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+Node *connect(Node *root)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    queue<Node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        int n = q.size();
+        Node *prev = NULL;
+        for (int i = 0; i < n; i++)
+        {
+            auto top = q.front();
+            q.pop();
+            top->next = prev;
+            prev = top;
+            if (top->right)
+            {
+                q.push(top->right);
+            }
+            if (top->left)
+            {
+                q.push(top->left);
+            }
+        }
+    }
+    return root;
+}
 int main()
 {
     return 0;
