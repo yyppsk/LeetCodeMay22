473eaedc5d758156338e0dcad6fa0c1bbfe91a11
Pranjal Pratap Singh <pranjalpratapsingh1@gmail.com>
2022-05-14 22:04:12 +0530

    FunctionDef

--- 743_Network_Delay_Time.cpp
+++ 743_Network_Delay_Time.cpp
@@ -1,10 +1,17 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
-void solve(){
-
-}
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+int networkDelayTime(vector<vector<int>> &times, int n, int k)
+{
+    vector<vector<pair<int, int>>> graph(n + 1);
+    vector<int> signals(n + 1, INT_MAX);
+
+    for (int i = 0; i < times.size(); i++)
+    {
+        int j = times[i][0];
+    }
+}
 int main()
 {
     return 0;
