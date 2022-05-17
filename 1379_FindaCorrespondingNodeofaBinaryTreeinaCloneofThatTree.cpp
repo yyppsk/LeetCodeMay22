b56a3582ffb2c963c06fca6c70ece8d9e52d65e8
Pranjal Pratap Singh <pranjalpratapsingh1@gmail.com>
2022-05-17 19:11:19 +0530

    if original != NULL

--- 1379_FindaCorrespondingNodeofaBinaryTreeinaCloneofThatTree.cpp
+++ 1379_FindaCorrespondingNodeofaBinaryTreeinaCloneofThatTree.cpp
@@ -6,6 +6,14 @@ public:
         TreeNode *res;
         return inorder(original, cloned, target, res);
     }
-    TreeNode *inorder(TreeNode *original, TreeNode *cloned, TreeNode *target, TreeNode *&res){
+    TreeNode *inorder(TreeNode *original, TreeNode *cloned, TreeNode *target, TreeNode *&res)
+    {
+        if (original != NULL)
+        {
+            inorder(original->left, cloned->left, target, res);
 
-    };
+            
+        }
+        return res;
+    }
+};
