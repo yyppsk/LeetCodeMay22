931d81e9bde07ba0188a348702397ff47af0335a
Pranjal Pratap Singh <pranjalpratapsingh1@gmail.com>
2022-05-17 19:12:13 +0530

    Structure

--- 1379_FindaCorrespondingNodeofaBinaryTreeinaCloneofThatTree.cpp
+++ 1379_FindaCorrespondingNodeofaBinaryTreeinaCloneofThatTree.cpp
@@ -1,25 +1,25 @@
-class Solution {
+class Solution
+{
 public:
-   
-    TreeNode* getTargetCopy(TreeNode* original, TreeNode* cloned, TreeNode* target) {
-       TreeNode*res;
-        return inorder(original,cloned,target,res);
-    }
-    
-    TreeNode*inorder(TreeNode* original, TreeNode* cloned,TreeNode*target,TreeNode*&res)
+    TreeNode *getTargetCopy(TreeNode *original, TreeNode *cloned, TreeNode *target)
     {
-        
-        if(original!=NULL)
+        TreeNode *res;
+        return inorder(original, cloned, target, res);
+    }
+
+    TreeNode *inorder(TreeNode *original, TreeNode *cloned, TreeNode *target, TreeNode *&res)
+    {
+
+        if (original != NULL)
         {
-            inorder(original->left,cloned->left,target,res);
-            
-                if(original==target)
-                {
-                    res=cloned;
-                }
-            
-            inorder(original->right,cloned->right,target,res);
-                
+            inorder(original->left, cloned->left, target, res);
+
+            if (original == target)
+            {
+                res = cloned;
+            }
+
+            inorder(original->right, cloned->right, target, res);
         }
         return res;
     }
